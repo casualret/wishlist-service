f6ed696fa7df8fa63defac6dfd59a37e789e8325
casualret <nikita.popov.1242@gmail.com>
2024-04-21 23:37:53 +0300

    ___

--- server/main.go
+++ server/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"server/internal/repo"
+)
+
 func main() {
 
+	repos, err := repo.InitRepo()
+	if err != nil {
+		log.Fatalf("Failed to initialize db: %v", err)
+	}
+
+	s := service.NewService(repos)
+	routes := handlers.NewHandlers(s)
+
+	server := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: routes.InitHandlers(),
+	}
+
+	log.Println("Starting server on localhost:8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
